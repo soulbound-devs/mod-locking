c2463feee064fa6aabefd642872290df2ec5ad87
vakror <vaylor2027@gmail.com>
2023-07-16 15:46:29 -0400

    Rename mixin

--- src/main/java/net/vakror/mod_locking/mixin/SlotMixin.java
+++ src/main/java/net/vakror/mod_locking/mixin/SlotMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.List;
 
 @Mixin(Slot.class)
-public abstract class RecipeMixin {
+public abstract class SlotMixin {
     @Shadow
     public abstract boolean hasItem();
 
