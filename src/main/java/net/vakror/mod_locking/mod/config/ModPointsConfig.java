4dc3f667026143834682f6cfa21c498972fdd4ed
vakror <vaylor2027@gmail.com>
2023-07-18 23:12:24 -0400

    remove unused imports

--- src/main/java/net/vakror/mod_locking/mod/config/ModPointsConfig.java
+++ src/main/java/net/vakror/mod_locking/mod/config/ModPointsConfig.java
@@ -1,12 +1,7 @@
 package net.vakror.mod_locking.mod.config;
 
 import com.google.gson.annotations.Expose;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.vakror.mod_locking.mod.point.IntColorPoint;
 import net.vakror.mod_locking.mod.point.ModPoint;
-import net.vakror.mod_locking.mod.point.RGBPoint;
-import net.vakror.mod_locking.mod.point.obtain.RightClickItemObtainMethod;
 
 import java.util.ArrayList;
 import java.util.List;
