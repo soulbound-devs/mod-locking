2de56b2b450a2ee145616d4bfe7be57a501fbecb
vakror <vaylor2027@gmail.com>
2023-07-26 15:53:40 -0400

    render overlay even in creative

--- src/main/java/net/vakror/mod_locking/PointOverlay.java
+++ src/main/java/net/vakror/mod_locking/PointOverlay.java
@@ -11,23 +11,18 @@ import net.vakror.mod_locking.mod.point.ModPoint;
 
 public class PointOverlay {
     public static final IGuiOverlay POINT_HUD = ((gui, graphics, partialTick, width, height) -> {
-        if (gui.shouldDrawSurvivalElements()) {
-            int x = width / 2;
-            int y = height;
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-
-            int i = 1;
-            for (ModPoint point : ModConfigs.POINTS.points) {
-                assert Minecraft.getInstance().player != null;
-                if (Minecraft.getInstance().player.getCapability(ModTreeProvider.MOD_TREE).orElse(new ModTreeCapability()).getPoints().get(point.name) != null) {
-                    int amount = Minecraft.getInstance().player.getCapability(ModTreeProvider.MOD_TREE).orElse(new ModTreeCapability()).getPoints().get(point.name);
-                    String nameString = amount + " " + (amount > 1 ? point.pluralName : point.name);
-                    int nameWidth = Minecraft.getInstance().font.width(nameString);
-                    graphics.drawString(Minecraft.getInstance().font, nameString, width - nameWidth - 10, (12 * i) - 6, point.getColor(), false);
-                    i++;
-                }
+        int i = 1;
+        for (ModPoint point : ModConfigs.POINTS.points) {
+            assert Minecraft.getInstance().player != null;
+            if (Minecraft.getInstance().player.getCapability(ModTreeProvider.MOD_TREE).orElse(new ModTreeCapability()).getPoints().get(point.name) != null) {
+                int amount = Minecraft.getInstance().player.getCapability(ModTreeProvider.MOD_TREE).orElse(new ModTreeCapability()).getPoints().get(point.name);
+                String nameString = amount + " " + (amount > 1 ? point.pluralName : point.name);
+                int nameWidth = Minecraft.getInstance().font.width(nameString);
+                graphics.drawString(Minecraft.getInstance().font, nameString, width - nameWidth - 10, (12 * i) - 6, point.getColor(), false);
+                i++;
             }
         }
     });
